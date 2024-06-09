c416078bcb0bbae68e6b21c5bfa1a2c5f5cd2097
kiramat85 <166742183+kiramat85@users.noreply.github.com>
2024-06-09 13:43:45 -0400

    Add files via upload

---
+++ exercises/ObjectExercises.js
@@ -0,0 +1,70 @@
+let superChimpOne = {
+   name: "Chad",
+   species: "Chimpanzee",
+   mass: 9,
+   age: 6
+};
+
+let salamander = {
+   name: "Lacey",
+   species: "Axolotl Salamander",
+   mass: 0.1,
+   age: 5
+};
+
+
+// After you have created the other object literals, add the astronautID property to each one.
+
+// Add a move method to each animal object
+
+// Create an array to hold the animal objects.
+
+let superChimpTwo = {
+   name: "Brad",
+   species: "Chimpanzee",
+   mass: 11,
+   age: 6,
+   astronautID: 3,
+   move: function () { return Math.floor(Math.random() * 11) }
+};
+
+let dog = {
+   name: "Leroy",
+   species: "Beagle",
+   mass: 14,
+   age: 5,
+   astronautID: 4,
+   move: function () { return Math.floor(Math.random() * 11) }
+};
+
+let waterBear = {
+   name: "Almina",
+   species: "Tardigrade",
+   mass: 0.0000000001,
+   age: 1,
+   astronautID: 5,
+   move: function () { return Math.floor(Math.random() * 11) }
+};
+
+// Print out the relevant information about each animal.
+
+let crew = [superChimpOne, superChimpTwo, salamander, dog, waterBear];
+
+
+let crew2 = [superChimpOne, superChimpTwo, salamander, dog, waterBear];
+
+// Start an animal race!
+
+function fitnessTest(candidates) {
+   let results = [], numSteps, turns;
+   for (let i = 0; i < candidates.length; i++) {
+      numSteps = 0;
+      turns = 0;
+      while (numSteps < 20) {
+         numSteps += candidates[i].move();
+         turns++;
+      }
+      results.push(`${candidates[i].name} took ${turns} turns to take 20 steps.`);
+   }
+   return results;
+}
